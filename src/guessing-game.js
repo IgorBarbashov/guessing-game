1c980300ddb9418a730bcb2a4d00a1a9d90f1ab7
isbnn <isb.cv@yandex.ru>
2019-09-25 00:39:04 +0300

    guessing-game/feat: complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,22 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-    }
+  guess() {
+    return Math.ceil(this.min + (this.max - this.min) / 2);
+  }
 
-    guess() {
+  lower() {
+    this.max = Math.ceil(this.max - (this.max - this.min) / 2);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = Math.ceil(this.min + (this.max - this.min) / 2);
+  }
 }
 
 module.exports = GuessingGame;
